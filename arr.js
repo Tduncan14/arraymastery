318b455013b8e6e4a4e38959495f8cd0f11c76c2
Treek <nbatre1@yahoo.com>
2020-08-02 17:20:29 -0500

    keep moving forward

---
+++ arr.js
@@ -0,0 +1,24 @@
+
+let arr1 = [];
+
+
+let arr2 = [1,2,3,4,5];
+
+
+/// array constructor  
+
+
+let arr3 = new Array();
+
+
+let arr4 = new Array(10);
+
+
+let arr5 = [,,,,,,,,,,,,]
+
+
+let arr6 = new Array(1,2,3,4,true,false);
+
+
+console.log(arr5);
+console.log(arr6);
