519d9cf1c278043e5d4e6724c85a9ef49aa98064
Treek <nbatre1@yahoo.com>
2020-08-19 16:03:33 -0500

    keep moving forward

---
+++ arrOb.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3]
+
+let obj = {
+
+    1:'one',
+    2:'two',
+    3:'three'
+}
+
+
+
+for( let val in obj){
+
+    console.log(obj[val])
+}
---
+++ arrlike.js
@@ -0,0 +1,35 @@
+
+
+let map = new Map()
+
+map.set(1,'one')
+map.set(2,'two')
+map.set(3,'three')
+
+
+
+let mapArray = [...map]
+
+
+console.log(mapArray,'map arry');
+
+
+let maps = Array.isArray(mapArray);
+
+console.log(maps);
+
+
+//spread opperation converts arr like sctures into array
+
+
+
+let Arry2 = Array.from(mapArray);
+
+
+console.log(Array.isArray(Arry2),'line 29')
+
+
+
+
+
+
